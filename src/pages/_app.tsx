c0e106fd250078a8ec43bab6293b6bd55b6f84ef
Victor Maya <victor.maya42@gmail.com>
2022-11-29 18:42:28 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <meta charSet="utf-8" />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default App
